135847875f893a34c8ffa958f4f36e6490fdbe78
Amanda Charles <ancharles11@gmail.com>
2016-12-14 08:51:11 -0800

    completed assignment requirements

--- httpServer.js
+++ httpServer.js
@@ -37,7 +37,59 @@ else if (req.method === 'GET' && req.url === '/pets/0') {
       const petJSON = JSON.stringify(pets[0]);
 
     res.setHeader('Content-Type', 'application/json');
-    res.end(petsJSON)
+    res.end(petJSON)
+  })
+}
+else if (req.method === 'GET' && req.url === '/pets/1') {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+
+      res.statusCode = 500;
+      res.setHeader = ('Content-Type', 'text/plain');
+      res.end = ('Internal Server Error');
+
+      return;
+    }
+      const pets = JSON.parse(petsJSON);
+      const petJSON = JSON.stringify(pets[1]);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(petJSON)
+  })
+}
+else if (req.method === 'GET' && req.url === '/pets/2') {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+
+      res.statusCode = 500;
+      res.setHeader = ('Content-Type', 'text/plain');
+      res.end = ('Internal Server Error');
+
+      return;
+    }
+
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found')
+  })
+}
+else if (req.method === 'GET' && req.url === '/pets/-1') {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+
+      res.statusCode = 500;
+      res.setHeader = ('Content-Type', 'text/plain');
+      res.end = ('Internal Server Error');
+
+      return;
+    }
+
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found')
   })
 }
 else {
@@ -53,54 +105,5 @@ server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-
-// else if (req.method === 'GET' && req.url === '/guests/0') {
-//   fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
-//     if (err) {
-//       console.error(err.stack);
-//
-//       res.statusCode = 500;
-//       res.setHeader('Content-Type', 'text/plain');
-//       res.end('Internal Server Error');
-//
-//       return;
-//     }
-// // parse the json file so you can access it
-//     const guests = JSON.parse(guestsJSON);
-// // then stringify to return JSON format data, the const here should be singular
-//     const guestJSON = JSON.stringify(guests[0]);
-//
-//     res.setHeader('Content-Type', 'application/json');
-// // put the guest of interest in the body of the response using res.end
-//     res.end(guestJSON);
-//   });
-// }
-// else if (req.method === 'GET' && req.url === '/guests/1') {
-//     fs.readFile(guestsPath, 'utf8', (err, guestsJSON) => {
-//       if (err) {
-//         console.error(err.stack);
-//
-//         res.statusCode = 500;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.end('Internal Server Error');
-//
-//         return;
-//       }
-//
-//       const guests = JSON.parse(guestsJSON);
-//       const guestJSON = JSON.stringify(guests[1]);
-//
-//       res.setHeader('Content-Type', 'application/json');
-//       res.end(guestJSON);
-//     });
-//   }
-// else {
-//   res.statusCode = 404;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Not found');
-// }
-// });
-//
-//
 
 module.exports = server;
